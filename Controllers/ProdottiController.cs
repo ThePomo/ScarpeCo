a0305176ec25ed3ded8957085f38bfd250048c1c
Giuseppe <Giuseppe@WIN-SOLSLKA85KI>
2025-02-21 12:43:24 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/ProdottiController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class ProdottiController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Prodotto> prodotti = ProdottiRepository.GetProdotti();
+        return View(prodotti);
+    }
+
+    public IActionResult Dettagli(int id)
+    {
+        Prodotto prodotto = ProdottiRepository.GetProdottoById(id);
+        if (prodotto == null)
+        {
+            return NotFound();
+        }
+        return View(prodotto);
+    }
+
+    public IActionResult Modifica(int id)
+    {
+        Prodotto prodotto = ProdottiRepository.GetProdottoById(id);
+        if (prodotto == null)
+        {
+            return NotFound();
+        }
+        return View(prodotto);
+    }
+
+    [HttpPost]
+    public IActionResult SalvaModifica(Prodotto modificato)
+    {
+        Prodotto prodottoEsistente = ProdottiRepository.GetProdottoById(modificato.Id);
+        if (prodottoEsistente == null)
+        {
+            return NotFound();
+        }
+
+        prodottoEsistente.Nome = modificato.Nome;
+        prodottoEsistente.Prezzo = modificato.Prezzo;
+        prodottoEsistente.Descrizione = modificato.Descrizione;
+        prodottoEsistente.ImmagineCopertina = modificato.ImmagineCopertina;
+
+        return RedirectToAction("Index");
+    }
+}
